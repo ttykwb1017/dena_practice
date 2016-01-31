86bdc4bb50257f7848fbcb019e6b57dafc7f5abe
ttykwb1017 <t_kawabe@fm.me.es.osaka-u.ac.jp>
2016-01-31 16:36:45 +0900

    add new file

--- lib/Schedular/Web/Dispatcher.pm
+++ lib/Schedular/Web/Dispatcher.pm
@@ -1,29 +1,33 @@
 package Schedular::Web::Dispatcher;
+# 文法チェック
 use strict;
 use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::RouterBoom;
 
-any '/' => sub {
+# my $xx; スカラ変数の定義
+# my @xx; 配列変数の定義
+# $i++ iをインクリメント
+get '/' => sub {
     my ($c) = @_;
-    my $counter = $c->session->get('counter') || 0;
-    $counter++;
-    $c->session->set('counter' => $counter);
-    return $c->render('index.tx', {
-        counter => $counter,
+    return $c->render('index.tx');
+};
+
+get '/user' => sub{
+    my ($c) = @_;
+    my $namae = "Tetsuya Kawabe";
+    my $nenrei = "23";
+    my $syozoku = "Mech. Eng. Osaka Univ.";
+    my $syumi = "cycling, watching tennis games";
+
+    return $c->render('user.tx', {
+        namae => $namae,
+        nenrei => $nenrei,
+        syozoku => $syozoku,
+        syumi => $syumi,
     });
-};
-
-post '/reset_counter' => sub {
-    my $c = shift;
-    $c->session->remove('counter');
-    return $c->redirect('/');
-};
-
-post '/account/logout' => sub {
-    my ($c) = @_;
-    $c->session->expire();
-    return $c->redirect('/');
+ 
+ 
 };
 
 1;
